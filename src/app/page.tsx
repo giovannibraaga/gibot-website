82807b9ea524cfa99792379e3bf3c33970b7079a
giovannibraaga <canaltamojuntooooo@gmail.com>
2024-06-19 12:55:12 -0300

    feat: created a hero section, used animated text.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
